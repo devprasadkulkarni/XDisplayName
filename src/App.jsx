40c693ba5c1e44b35ef6ddb1caa6857964606089
Prasad Kulkarni <dev.prasad.kulkarni@gmail.com>
2025-10-03 20:27:15 +0530

    error-fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,54 +5,53 @@ const App = () => {
         firstName: "",
         lastName: "",
     });
-    const [isSubmitted, setIsSubmitted] = useState(false);
+    const [fullName, setFullName] = useState("");
 
     const handleChange = (e) => {
         const { name, value } = e.target;
-        console.log(name, value);
         setData((prev) => ({ ...prev, [name]: value }));
     };
 
     const handleSubmit = (e) => {
         e.preventDefault();
         if (data.firstName.trim() && data.lastName.trim()) {
-            setIsSubmitted(true);
+            setFullName(`${data.firstName} ${data.lastName}`);
         } else {
-            setIsSubmitted(false);
+            setFullName(""); // don’t display anything if fields are empty
         }
     };
 
     return (
-        <form onSubmit={handleSubmit} className="mx-8">
-            <h1 className="text-4xl my-8 ">Full Name Display</h1>
+        <form onSubmit={handleSubmit}>
+            <h1>Full Name Display</h1>
             <span>First Name: </span>
             <input
                 className="border"
-                onChange={handleChange}
+                type="text"
                 name="firstName"
+                placeholder="First Name"
                 value={data.firstName}
+                onChange={handleChange}
                 required
             />
             <br />
-            <br />
+
             <span>Last Name: </span>
             <input
                 className="border"
-                onChange={handleChange}
+                type="text"
                 name="lastName"
+                placeholder="Last Name"
                 value={data.lastName}
+                onChange={handleChange}
                 required
             />
-            <br />
             <br />
             <button className="border px-2" type="submit">
                 Submit
             </button>
-            <p>
-                {isSubmitted
-                    ? `Full Name: ${data.firstName} ${data.lastName}`
-                    : ""}
-            </p>
+
+            {fullName && <p>Full Name: {fullName}</p>}
         </form>
     );
 };
